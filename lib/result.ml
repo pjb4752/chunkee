9dc755f8bb101bbdd57a92ad6706514127a4c175
pjb4752 <pjb4752@gmail.com>
2018-03-19 17:11:54 -0400

    fill out result module

--- lib/result.ml
+++ lib/result.ml
@@ -1,4 +1,13 @@
+let is_ok = function
+  | Ok _ -> true
+  | Error _ -> false
+
+let is_error r = not (is_ok r)
+
 let map fn = function
   | Ok v -> Ok (fn v)
   | Error e -> Error e
 
+let flat_map fn = function
+  | Ok v -> fn v
+  | Error e -> Error e
--- lib/result.mli
+++ lib/result.mli
@@ -1,1 +1,7 @@
+val is_ok: ('a, 'b) result -> bool
+
+val is_error: ('a, 'b) result -> bool
+
 val map: ('a -> 'b) -> ('a, 'c) result -> ('b, 'c) result
+
+val flat_map: ('a -> ('b, 'c) result) -> ('a, 'c) result -> ('b, 'c) result
