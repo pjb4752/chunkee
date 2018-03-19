94b0fce0ac5d5dee22417e919dfd67b88065fa0e
pjb4752 <pjb4752@gmail.com>
2018-03-19 17:39:54 -0400

    clean up node printing

--- lib/node.ml
+++ lib/node.ml
@@ -10,13 +10,15 @@ type t =
   | Fn of (Param.t list * t)
 
 let rec to_string node =
-  let string_of_def n e  = Module.Var.Name.to_string n ^ ", " ^ to_string e in
+  let string_of_def name expr =
+    let name = Module.Var.Name.to_string name in
+    sprintf "Def(%s, %s)" name (to_string expr) in
   let string_of_fn params body =
     let params = String.concat ", " (List.map Param.to_string params) in
     sprintf "Fn(Params(%s), %s)" params (to_string body) in
   match node with
-  | NumLit n -> "NumLit(" ^ string_of_float n ^ ")"
-  | StrLit s -> "StrLit(" ^ s ^ ")"
-  | SymLit s -> "SymLit(" ^ s ^ ")"
-  | Def (n, e) -> "Def(" ^ string_of_def n e ^ ")"
+  | NumLit n -> sprintf "NumLit(%.2f)" n
+  | StrLit s -> sprintf "StrLit(%s)" s
+  | SymLit s -> sprintf "SymLit(%s)" s
+  | Def (name, expr) -> string_of_def name expr
   | Fn (params, body) -> string_of_fn params body
