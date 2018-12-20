822f5ccd5552a6d378376ad4634714b6b7748c80
pjb4752 <pjb4752@gmail.com>
2018-12-20 17:19:45 -0500

    use read_list to track position of forms

--- lib/lex.ml
+++ lib/lex.ml
@@ -1,6 +1,8 @@
 open Printf
 open Thwack.Extensions
 
+module Read_list = Thwack.Read_list
+
 exception SyntaxError of string
 
 module Form = struct
@@ -30,16 +32,16 @@ let lower_case = ['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l';
 let upper_case = ['A'; 'B'; 'C'; 'D'; 'E'; 'F'; 'G'; 'H'; 'I'; 'J'; 'K'; 'L';
   'M'; 'N'; 'O'; 'P'; 'Q'; 'R'; 'S'; 'T'; 'U'; 'V'; 'W'; 'X'; 'Y'; 'Z']
 let operators = ['+'; '-'; '*'; '/'; '>'; '<'; '=']
+let punctuation = ['!'; '?'; '.'; ':']
 
 let symbol_starting_chars = '_' :: (List.append lower_case operators)
 let symbol_chars =
-  symbol_starting_chars @ upper_case @ digits @ ['!'; '?'; '.'; ':']
+  symbol_starting_chars @ upper_case @ digits @ punctuation
 
 let cons_starting_chars = upper_case
-let cons_chars =
-  cons_starting_chars @ lower_case @ digits
+let cons_chars = cons_starting_chars @ lower_case @ digits
 
-let is_char_of set ch = List.exists (fun c -> c = ch) set
+let is_char_of set chr = List.exists (fun c -> c = chr) set
 
 let is_blank = is_char_of whitespace
 let is_digit = is_char_of digits
@@ -53,92 +55,112 @@ let is_vec_open = is_char_of ['[']
 
 let string_to_number s = Form.Number (float_of_string s)
 
-let rec remove_blank = function
-  | [] -> []
-  | x :: xs when not (is_blank x) -> x :: xs
-  | _ :: xs -> remove_blank xs
+let rec remove_blank input =
+  Read_list.(
+    match input with
+    | [] -> []
+    | x :: xs when not (is_blank x.value) -> x :: xs
+    | _ :: xs -> remove_blank xs
+  )
 
 let lex_form input terminal_fn test_fn =
-  let rec lex_form' input output =
-    match input with
-    | [] -> terminal_fn input output
-    | x :: _ when test_fn x -> terminal_fn input output
-    | x :: xs -> lex_form' xs (String.append_char output x) in
-  lex_form' input ""
+  Read_list.(
+    let rec lex_form' input output =
+      match input with
+      | [] -> terminal_fn input output
+      | { value } :: xs when test_fn value -> terminal_fn input output
+      | { value } :: xs -> lex_form' xs (String.append_char output value) in
+    lex_form' input ""
+  )
 
 (* TODO: needs better handling of decimal points, preceding +/- *)
-let lex_num input =
-  let terminal_fn i o = (i, string_to_number o)
-  and is_not_digit = (fun c -> not (is_digit c)) in
+let lex_num line_num char_num input =
+  let terminal_fn input output = (input, string_to_number output) in
+  let is_not_digit = (fun c -> not (is_digit c)) in
   lex_form input terminal_fn is_not_digit
 
-let lex_symbol input =
-  let terminal_fn i o = (i, Form.Symbol o)
-  and is_not_symbol_char = (fun c -> not (is_symbol_char c)) in
+let lex_symbol line_num char_num input =
+  let terminal_fn input output = (input, Form.Symbol output) in
+  let is_not_symbol_char = (fun c -> not (is_symbol_char c)) in
   lex_form input terminal_fn is_not_symbol_char
 
-let lex_cons input =
-  let terminal_fn i o = (i, Form.Cons o)
-  and is_not_cons_char = (fun c -> not (is_cons_char c)) in
+let lex_cons line_num char_num input =
+  let terminal_fn input output = (input, Form.Cons output) in
+  let is_not_cons_char = (fun c -> not (is_cons_char c)) in
   lex_form input terminal_fn is_not_cons_char
 
 let lex_delimited input start delimiter terminal_fn input_fn =
-  let rec lex_delimited' input output =
-    match input with
-    | [] -> raise (SyntaxError (Printf.sprintf "expecting '%c', none found" delimiter))
-    | x :: xs when x = delimiter -> (xs, terminal_fn output)
-    | _ :: _ ->
-        let (new_input, new_output) = input_fn input output in
-        lex_delimited' new_input new_output in
-  lex_delimited' input start
+  Read_list.(
+    let rec lex_delimited' input output =
+      match input with
+      | [] ->
+          let message = sprintf "expecting '%c', none found" delimiter in
+          raise (SyntaxError message)
+      | { value } :: xs when value = delimiter -> (xs, terminal_fn output)
+      | { value } :: xs ->
+          let (new_input, new_output) = input_fn input output in
+          lex_delimited' new_input new_output in
+    lex_delimited' input start
+  )
 
-let lex_string input =
-  let terminal_fn = (fun out -> Form.String out)
-  and input_fn i out =
-    match i with
+let lex_string line_num char_num input =
+  let terminal_fn = (fun output -> Form.String output) in
+  let input_fn input out =
+    match input with
     | [] -> assert false
-    | x :: xs -> (xs, String.append_char out x) in
-  lex_delimited (List.tl input) "" '"' terminal_fn input_fn
+    | { Read_list.value } :: xs -> (xs, String.append_char out value) in
+  match input with
+  | [] -> assert false
+  | x :: xs -> lex_delimited xs "" '"' terminal_fn input_fn
 
 let lex_collection lex_fn input terminal_fn terminal_char =
-  let input_fn = (fun i out ->
-    match i with
+  Read_list.(
+    let input_fn = (fun input output ->
+      match input with
+      | [] -> assert false
+      | { value } :: xs when (is_blank value) ->
+          let new_input = remove_blank input in (new_input, output)
+      | { value } :: xs ->
+          let (new_input, form) = lex_fn input in
+          (new_input, form :: output)) in
+    match input with
     | [] -> assert false
-    | x :: _ when (is_blank x) -> let new_i = remove_blank i in (new_i, out)
-    | _ :: _ -> let (new_i, f) = lex_fn i in (new_i, f :: out)) in
-  lex_delimited (List.tl input) [] terminal_char terminal_fn input_fn
+    | x :: xs -> lex_delimited xs [] terminal_char terminal_fn input_fn
+  )
 
-let lex_list lex_fn input =
-  let terminal_fn = (fun out -> Form.List (List.rev out)) in
+let lex_list lex_fn line_num char_num input =
+  let terminal_fn = (fun output -> Form.List (List.rev output)) in
   lex_collection lex_fn input terminal_fn ')'
 
-let lex_vec lex_fn input =
-  let terminal_fn = (fun out -> Form.Vec (List.rev out)) in
+let lex_vec lex_fn line_num char_num input =
+  let terminal_fn = (fun output -> Form.Vec (List.rev output)) in
   lex_collection lex_fn input terminal_fn ']'
 
 let rec try_lex input =
-  let c = List.hd input in
-  if is_digit c then lex_num input
-  else if is_string_delim c then lex_string input
-  else if is_symbol_starting_char c then lex_symbol input
-  else if is_cons_starting_char c then lex_cons input
-  else if is_list_open c then lex_list try_lex input
-  else if is_vec_open c then lex_vec try_lex input
-  else let bad_input = String.from_chars input in
+  let { Read_list.value; line_num; char_num } = List.hd input in
+  if is_digit value then lex_num line_num char_num input
+  else if is_string_delim value then lex_string line_num char_num input
+  else if is_symbol_starting_char value then lex_symbol line_num char_num input
+  else if is_cons_starting_char value then lex_cons line_num char_num input
+  else if is_list_open value then lex_list try_lex line_num char_num input
+  else if is_vec_open value then lex_vec try_lex line_num char_num input
+  else
+    let chars = Read_list.(List.map (fun e -> e.value) input) in
+    let bad_input = String.from_chars chars in
     raise (SyntaxError (sprintf "unrecognized form '%s'" bad_input))
 
 let lex_forms input =
-  let rec lex_list' input forms =
-    match input with
-    | [] -> List.rev forms
-    | x :: xs when is_blank x -> lex_list' xs forms
-    | _ ->
-        let (new_input, new_form) = try_lex input in
-        lex_list' new_input (new_form :: forms) in
-  lex_list' input []
+  Read_list.(
+    let rec lex_list' input forms =
+      match input with
+      | [] -> List.rev forms
+      | { value } :: xs when is_blank value -> lex_list' xs forms
+      | xs ->
+          let (new_input, new_form) = try_lex input in
+          lex_list' new_input (new_form :: forms) in
+    lex_list' input []
+  )
 
-let lex_exn s = lex_forms @@ String.to_chars s
-
-let lex s =
-  try Ok (lex_exn s)
+let lex str =
+  try Ok (lex_forms @@ Read_list.from_string str)
   with SyntaxError e -> Error (Cmpl_err.SyntaxError e)
--- lib/lex.mli
+++ lib/lex.mli
@@ -12,6 +12,4 @@ module Form : sig
   val to_string: t -> string
 end
 
-val lex_exn: string -> Form.t list
-
 val lex: string -> (Form.t list, Cmpl_err.t) result
